160b40cc5dfacbad82b2a30a47801de61b65f980
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-07-20 18:13:52 +0300

    Add changes

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ public class Main {
         return S.get(index).getValue();
     }
 
+    private static int func(int S, int x) {
+        return (S - x) * x;
+    }
+
     static List<Map<String, Integer>> split(Map<String, Integer> set) {
         Objects.requireNonNull(set);
         List<Map.Entry<String, Integer>> S = set.entrySet().stream().toList();
@@ -24,37 +28,66 @@ public class Main {
         path[0][1][1] = -1;
 
         for (int i = 1; i < n; ++i) {
-            for (int k = 0; k <= i + 1; ++k) {
+            dp[i][0][0] = dp[i][0][1] = 0;
+            path[i][0][0] = path[i][0][1] = -1;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            for (int k = 1; k <= i + 1; ++k) {
                 // skip current item (t=0)
-                {
+
+                for (int t = 0; t <= 1; ++t) {
+                    int p = price(S, i);
+                    for (int t0 = 0; t0 <= 1; ++t0) {
+                        int A = p * t + dp[i-1][k - t][t0];
+                        if (func(sum, dp[i][k][0]) < func(sum, A)) {
+                            dp[i][k][0] = A;
+                            path[i][k][0] = t0;
+                        }
+                    }
+
+                }
+
+                for (int t0 = 0; t0 <= 1; ++t0) {
+                    int A = dp[i-1][k][t0];
+                    if (func(sum, dp[i][k][0]) < func(sum, A)) {
+                        dp[i][k][0] = A;
+                        path[i][k][0] = t0;
+                    }
+                }
+                /*{
                     int A0 = dp[i-1][k][0];
                     int A1 = dp[i-1][k][1];
 
                     if ((sum - A0) * A0 < (sum - A1) * A1) {
-                        dp[i][k][0] = A1; // dp[i-1][k][1];
+                        dp[i][k][0] = A1;
                         path[i][k][0] = 1;
                     }
                     else {
-                        dp[i][k][0] = A0; // dp[i-1][k][0];
+                        dp[i][k][0] = A0;
                         path[i][k][0] = 0;
                     }
-                }
+                }*/
 
                 // take current item (t=1)
-                if (k > 0) {
-                    int p = price(S, i);
-                    int A0 = p + dp[i-1][k-1][0];
-                    int A1 = p + dp[i-1][k-1][1];
-
-                    if ((sum - A0) * A0 < (sum - A1) * A1) {
-                        dp[i][k][1] = A1;
-                        path[i][k][1] = 1;
-                    }
-                    else {
-                        dp[i][k][1] = A0;
-                        path[i][k][1] = 0;
+                for (int t0 = 0; t0 <= 1; ++t0) {
+                    int A = p + dp[i-1][k-1][t0];
+                    if (func(sum, dp[i][k][1]) < func(sum, A)) {
+                        dp[i][k][0] = A;
+                        path[i][k][0] = t0;
                     }
                 }
+                /*int A0 = p + dp[i-1][k-1][0];
+                int A1 = p + dp[i-1][k-1][1];
+
+                if ((sum - A0) * A0 < (sum - A1) * A1) {
+                    dp[i][k][1] = A1;
+                    path[i][k][1] = 1;
+                }
+                else {
+                    dp[i][k][1] = A0;
+                    path[i][k][1] = 0;
+                }*/
             }
         }
 
